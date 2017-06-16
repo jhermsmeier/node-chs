84e8fd4807a2b0d1f42bba228c4051aad756ac69
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-06-16 19:20:20 +0200

    test: Add value range tests

--- test/chs.js
+++ test/chs.js
@@ -3,43 +3,69 @@ var CHS = require( '../' )
 
 describe( 'CHS', function() {
 
-  describe( 'should map the right LBAs corresponding to geometry', function() {
+  describe( 'Value ranges', function() {
+
+    specify( 'CHS(0/0/0)', function() {
+      var chs = new CHS( 0, 0, 0 )
+      assert.deepEqual( CHS.fromBuffer( chs.toBuffer() ), chs )
+    })
+
+    specify( 'CHS(8/4/32)', function() {
+      var chs = new CHS( 8, 4, 32 )
+      assert.deepEqual( CHS.fromBuffer( chs.toBuffer() ), chs )
+    })
+
+    specify( 'CHS(1023/254/63)', function() {
+      var chs = new CHS( 1023, 254, 63 )
+      assert.deepEqual( CHS.fromBuffer( chs.toBuffer() ), chs )
+    })
+
+    specify( 'CHS(1234/268/76) (overflow)', function() {
+      var chs = new CHS( 1234, 268, 76 )
+      var expected = new CHS( 1234 & 0x03FF, 268 & 0xFF, 76 & 0x3F )
+      var actual = CHS.fromBuffer( chs.toBuffer() )
+      assert.deepEqual( actual, expected )
+    })
+
+  })
+
+  describe( 'Map LBAs corresponding to geometry', function() {
 
     // For geometry 1020 16 63 of a disk with
     // 1028160 sectors CHS 3 2 1 is LBA  3150=(3*16+2)* 63
-    it( 'Geometry 1020 16 63', function() {
+    specify( 'Geometry 1020 16 63', function() {
       var chs = new CHS( 3, 2, 1 )
       assert.equal( chs.toLBA( 16, 63 ), 3150 )
     })
 
     // For geometry 1008 4 255 of a disk with
     // 1028160 sectors CHS 3 2 1 is LBA  3570=(3*4+2)*255
-    it( 'Geometry 1008 4 255', function() {
+    specify( 'Geometry 1008 4 255', function() {
       var chs = new CHS( 3, 2, 1 )
       assert.equal( chs.toLBA( 4, 255 ), 3570 )
     })
 
     // For geometry 64 255 63 of a disk with
     // 1028160 sectors CHS 3 2 1 is LBA 48321=(3*255+2)* 63
-    it( 'Geometry 64 255 63', function() {
+    specify( 'Geometry 64 255 63', function() {
       var chs = new CHS( 3, 2, 1 )
       assert.equal( chs.toLBA( 255, 63 ), 48321 )
     })
 
     // For geometry 2142 15 32 of a disk with
     // 1028160 sectors CHS 3 2 1 is LBA  1504=(3*15+2)* 32
-    it( 'Geometry 2142 15 32', function() {
+    specify( 'Geometry 2142 15 32', function() {
       var chs = new CHS( 3, 2, 1 )
       assert.equal( chs.toLBA( 15, 32 ), 1504 )
     })
 
   })
 
-  describe( 'should map the right CHS to a given LBA with geometry', function() {
+  describe( 'Map CHS to a given LBA with geometry', function() {
 
     // For geometry 1020 16 63 of a disk with
     // 1028160 sectors CHS 3 2 1 is LBA  3150=(3*16+2)* 63
-    it( 'Geometry 1020 16 63', function() {
+    specify( 'Geometry 1020 16 63', function() {
       var chs = new CHS()
       chs.setLBA( 3150, 16, 63 )
       assert.equal( chs.cylinder, 3 )
@@ -49,7 +75,7 @@ describe( 'CHS', function() {
 
     // For geometry 1008 4 255 of a disk with
     // 1028160 sectors CHS 3 2 1 is LBA  3570=(3*4+2)*255
-    it( 'Geometry 1008 4 255', function() {
+    specify( 'Geometry 1008 4 255', function() {
       var chs = new CHS()
       chs.setLBA( 3570, 4, 255 )
       assert.equal( chs.cylinder, 3 )
@@ -59,7 +85,7 @@ describe( 'CHS', function() {
 
     // For geometry 64 255 63 of a disk with
     // 1028160 sectors CHS 3 2 1 is LBA 48321=(3*255+2)* 63
-    it( 'Geometry 64 255 63', function() {
+    specify( 'Geometry 64 255 63', function() {
       var chs = new CHS()
       chs.setLBA( 48321, 255, 63 )
       assert.equal( chs.cylinder, 3 )
@@ -69,7 +95,7 @@ describe( 'CHS', function() {
 
     // For geometry 2142 15 32 of a disk with
     // 1028160 sectors CHS 3 2 1 is LBA  1504=(3*15+2)* 32
-    it( 'Geometry 2142 15 32', function() {
+    specify( 'Geometry 2142 15 32', function() {
       var chs = new CHS()
       chs.setLBA( 1504, 15, 32 )
       assert.equal( chs.cylinder, 3 )
